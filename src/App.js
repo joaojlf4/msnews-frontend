c2bf25687519a0ff7de8f6d806787c0ad9c0d741
TheSniker007 <joaojl2099@gmail.com>
2020-04-04 16:29:22 -0300

    Not found page fix and Switch added to routes.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Main from './pages/Main';
 import Write from './pages/Write';
 import NotFound from './pages/NotFound';
@@ -12,10 +12,13 @@ function App() {
     <>
       <GlobalStyle />
       <Router>
-        <Route exact path="/" component={Main} />
-        <Route exact path="/news/:title" children={<NewPage />}/>
-        <Route exact path="/write" component={Write} />
-        <Route exact path="/nao-encontrado" component={NotFound} />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/news/:title" children={<NewPage />}/>
+          <Route exact path="/write" component={Write} />
+          <Route exact path="/nao-encontrado" component={NotFound} />
+          <Route component={NotFound} />
+        </Switch>
       </Router>
     </>
   );
--- src/pages/NotFound/styles.js
+++ src/pages/NotFound/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  margin-top: 100px;
   display: flex;
   flex-direction: column;
   align-items: center;
